748c4c3a7a5ac50854c343b05424573111273ce2
HoJun Choi <nyhj962@gmail.com>
2023-06-14 02:33:29 +0900

    .

--- SwiftPS/Programmers/Level3/양과늑대.swift
+++ SwiftPS/Programmers/Level3/양과늑대.swift
@@ -5,6 +5,41 @@
 //  Created by HoJun on 2023/06/14.
 //
 
+/*
+    DFS + 백트래킹
+    :
+ */
+
 import Foundation
 
-
+func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
+    
+    var graph: [Int: [Int]] = [:]
+    var vst = Array(repeating: false, count: info.count)
+    var total = 0
+    
+    vst[0] = true
+    dfs(sheep: 1, wolf: 0)
+    
+    func dfs(sheep: Int, wolf: Int) {
+        if sheep <= wolf {
+            return
+        }
+        
+        total = max(sheep, total)
+        
+        for edge in edges {
+            let parent = edge[0]
+            let child = edge[1]
+            
+            if vst[parent] && !vst[child] {
+                vst[child] = true
+                let childAnimal = info[child] == 0 ? (1, 0) : (0, 1)
+                dfs(sheep: sheep + childAnimal.0, wolf: wolf + childAnimal.1)
+                vst[child] = false
+            }
+        }
+    }
+        
+    return total
+}
