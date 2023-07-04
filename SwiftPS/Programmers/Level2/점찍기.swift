30429594c98ac24dfbd0cc38f26475dc76ba261d
HoJun Choi <nyhj962@gmail.com>
2023-07-04 20:08:35 +0900

    .

---
+++ SwiftPS/Programmers/Level2/점찍기.swift
@@ -0,0 +1,21 @@
+//
+//  점찍기.swift
+//  SwiftPS
+//
+//  Created by HoJun on 2023/07/04.
+//
+
+import Foundation
+
+func solution(_ k:Int, _ d:Int) -> Int64 {
+    var result = 0
+    
+    for x in stride(from: 0, through: d, by: k) {
+        let y = Int(sqrt(pow(Double(d), 2.0) - pow(Double(x), 2.0)))
+        result += y / k
+    }
+    
+    result += d / k + 1
+    
+    return Int64(result)
+}
